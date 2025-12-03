5eb14e6d99759cc1b9b56f86017605c4daab3954
arash-nm <arashnm797@gmail.com>
2025-12-03 19:03:38 +0330

    init

---
+++ src/test/java/com/mr_arashnm/springstore/SpringStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mr_arashnm.springstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
