e03c7909f745c05ee9e36ca7e444fee148a9870f
arash-nm <arashnm797@gmail.com>
2026-01-10 01:40:51 +0330

    finish 6

---
+++ src/main/resources/db/migration/V1__initial_migration.sql
@@ -0,0 +1,20 @@
+create table users
+(
+    id          bigint auto_increment
+        primary key,
+    name        varchar(255) not null,
+    email       varchar(255) not null,
+    password    varchar(255) not null,
+);
+
+create table address
+(
+    id          bigint auto_increment
+        primary key,
+    street      varchar(255) not null,
+    city        varchar(255) not null,
+    zip         varchar(255) not null,
+    user_id     bigint       not null,
+    constraint address_users_id_fk
+        foreign key (user_id) refrences users (id)
+);
---
+++ src/main/resources/db/migration/v4__add_profiles_tables.sql
@@ -0,0 +1,9 @@
+create table profiles
+(
+    id              bigint primary key,
+    bio             text,
+    phone_number    varchar(15),
+    date_of_birth   DATE,
+    loyalty_points  int unsingned default 0,
+    foreign key id references users(id)
+);
