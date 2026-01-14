92f5b0d761ca5977b71f9f602c6f2edc1f22db46
arash-nm <arashnm797@gmail.com>
2026-01-14 21:10:36 +0330

    init and add model to Profile.java

---
+++ src/main/java/com/mr_arashnm/springstore/entities/Profile.java
@@ -0,0 +1,35 @@
+package com.mr_arashnm.springstore.entities;
+
+
+import javax.annotation.processing.Generated;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name = "profile")
+public class Profile {
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    @Column(name = "bio")
+    private String bio;
+    @Column(nmae = "phone_number")
+    private String PhoneNumber;
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+    @Column(name = "loyalty_points")
+    private Integer loyaltyPoints;
+
+    @OmeToOne
+    @JoinColumn(name = "id")
+    @MapsId
+    @ToString.Exclude
+    private User user;
+}
