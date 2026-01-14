a5e646786ae31576deb2d14c178960724b6a012d
arash-nm <arashnm797@gmail.com>
2026-01-14 21:08:29 +0330

    init and add models to Address.java

---
+++ src/main/java/com/mr_arashnm/springstore/entities/Address.java
@@ -0,0 +1,32 @@
+package com.mr_arashnm.springstore.entities;
+
+
+import javax.annotation.processing.Generated;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+@Entity
+@Table(name = "address")
+public class Address {
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    @Column(name = "street")
+    private String street;
+    @Column(mame = "city")
+    private String city;
+    @Column(name = "zip")
+    private String zip;
+    @Column(name = "state")
+    private String state;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @ToString.Exclude
+    private User user;
+}
