6de67163454871315d6176e22cb113687e682f4f
arash-nm <arashnm797@gmail.com>
2026-01-15 01:22:17 +0330

    cleaning

--- src/main/java/com/mr_arashnm/springstore/SpringStoreApplication.java
+++ src/main/java/com/mr_arashnm/springstore/SpringStoreApplication.java
@@ -1,20 +1,14 @@
 package com.mr_arashnm.springstore;
 
-import com.mr_arashnm.springstore.entities.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringStoreApplication {
 
 	public static void main(String[] args) {
-//		ApplicationContext context = SpringApplication.run(SpringStoreApplication.class, args);
-        var user = new User(1l, "name", "email", "password");
-        user.setName("john");
-        user.setEmail("Wdsc ed@gmail.com");
-        user.setPassword("password");
+		ApplicationContext context = SpringApplication.run(SpringStoreApplication.class, args);
 
 
     }
