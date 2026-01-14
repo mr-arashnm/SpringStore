e3c9c142c02623ac83de4972ba01e142a1b81af2
arash-nm <arashnm797@gmail.com>
2026-01-15 00:51:51 +0330

    add Category & Product entities

---
+++ src/main/java/com/mr_arashnm/springstore/entities/Product.java
@@ -0,0 +1,27 @@
+package com.mr_arashnm.springstore.entities;
+
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(sterategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @ManyToMany
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+}
