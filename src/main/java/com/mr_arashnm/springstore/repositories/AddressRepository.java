170324914541a210e7f7b9ae8138a743fb37dd8c
arash-nm <arashnm797@gmail.com>
2026-01-15 02:15:32 +0330

    init

---
+++ src/main/java/com/mr_arashnm/springstore/repositories/AddressRepository.java
@@ -0,0 +1,7 @@
+package com.mr_arashnm.springstore.repositories;
+
+import com.mr_arashnm.springstore.entities.Address;
+
+public interface AddressRepository extends CrudRepository<Address, Long> {
+
+}
---
+++ src/main/java/com/mr_arashnm/springstore/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.mr_arashnm.springstore.repositories;
+
+import com.mr_arashnm.springstore.entities.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
