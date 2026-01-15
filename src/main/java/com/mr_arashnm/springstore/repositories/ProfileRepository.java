457d235baac2798bfe9290dbf5e65d836b4e0a9d
arash-nm <arashnm797@gmail.com>
2026-01-15 03:42:08 +0330

    init

---
+++ src/main/java/com/mr_arashnm/springstore/repositories/ProfileRepository.java
@@ -0,0 +1,7 @@
+package com.mr_arashnm.springstore.repositories;
+
+import com.mr_arashnm.springstore.entities.Profile;
+
+
+public interface ProfileRepository extends CrudRepository<Profile, Long>{
+}
