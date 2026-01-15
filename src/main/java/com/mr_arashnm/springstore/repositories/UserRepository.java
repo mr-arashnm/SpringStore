25809d2747a05bd428130ad3d56f03940c80ae62
arash-nm <arashnm797@gmail.com>
2026-01-15 19:41:55 +0330

    add addresses to method

--- src/main/java/com/mr_arashnm/springstore/repositories/UserRepository.java
+++ src/main/java/com/mr_arashnm/springstore/repositories/UserRepository.java
@@ -5,6 +5,6 @@ import com.mr_arashnm.springstore.entities.User;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-    @EntityGraph(attributePaths = "tags")
+    @EntityGraph(attributePaths = {"tags", "addresses"})
     Optional<User> findByEmail(String email);
 }
