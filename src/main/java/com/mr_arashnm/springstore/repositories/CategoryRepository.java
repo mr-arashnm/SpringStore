4e1de0f1f631c2aad4d2adc39984a9283fa8a9ff
arash-nm <arashnm797@gmail.com>
2026-01-15 19:14:52 +0330

    init

---
+++ src/main/java/com/mr_arashnm/springstore/repositories/CategoryRepository.java
@@ -0,0 +1,6 @@
+package com.mr_arashnm.springstore.repositories;
+
+import com.mr_arashnm.springstore.entities.Category;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+}
